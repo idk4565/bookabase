349db06c4530432203ff184916a809aa12eba303
Siddhartha Juluru <31663833+archturtle@users.noreply.github.com>
2023-10-26 14:19:31 -0400

    api starter files

---
+++ api/src/test/java/com/bookabase/api/APIApplicationTests.java
@@ -0,0 +1,10 @@
+package com.bookabase.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APIApplicationTests {
+    @Test
+    void contextLoads() { }
+}
